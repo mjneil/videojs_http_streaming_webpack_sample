ace07b35ee78909396d0b373fdb598cd348c9435
Matthew Neil <mneil@mneil-retina-2016.vidmark.local>
2018-02-05 15:38:03 -0500

    bundle with webpack and browserify

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var videojs = require('video.js');
+
 require('@videojs/http-streaming');
 
 var player = videojs('player');
 
-player.src({ src: 'https://hslsslak-a.akamaihd.net/3303963094001/3303963094001_5147618278001_5147609827001.m3u8?pubId=3303963094001&videoId=5147609827001', type: 'application/x-mpegURL'});
+player.ready(function() {
+  player.src({
+    src: 'http://d2zihajmogu5jn.cloudfront.net/sintel/master.m3u8',
+    type: 'application/x-mpegURL'
+  });
+});
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
+var path = require('path');
+
 module.exports = {
   entry: './index.js',
   output: {
     path: __dirname,
-    filename: 'bundle.js'
-  }
+    filename: 'bundle-w.js'
+  },
+  // resolve: {
+  //   alias: {
+  //     'video.js': path.join(__dirname, 'node_modules/video.js')
+  //   }
+  // }
 };
